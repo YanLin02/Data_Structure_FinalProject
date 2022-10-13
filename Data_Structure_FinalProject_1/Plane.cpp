2a992de8792684f00a3fb7ca794870bb59053b57
Sun Yanlin <1310400098@qq.com>
2022-10-14 00:20:28 +0800

    完善了Plane类 补充等待时间功能 增加状态标识

--- Data_Structure_FinalProject_1/Plane.cpp
+++ Data_Structure_FinalProject_1/Plane.cpp
@@ -3,18 +3,35 @@
 ostream& operator<<(ostream& out, const Plane& p)
 {
 	out << "编号为：" << p.m_id << "的飞机";
-	if (p.m_timeRemaining > 0)
-		out << "尚未降落，剩余时间为" << p.m_timeRemaining << endl;
+	if (p.m_state == arriving)
+		out << "尚未降落，已经等待：" << p.m_waitingTime << "单位时间，剩余时间为" << p.m_timeRemaining << endl;
 	else
-		out << "等待起飞" << endl;
+		out << "等待起飞，已经等待：" << p.m_waitingTime << "单位时间" << endl;
 	return out;
 }
 
+Plane::Plane(int id, int t)
+	:m_id(id), m_timeRemaining(t), m_waitingTime(0)
+{
+	if (t < 0)
+		this->m_state = departing;
+	else
+		this->m_state = arriving;
+}
+
+
 Plane::Plane(int id, Plane_states state)
-	:m_id(id), m_waitingTime(0)
+	:m_id(id), m_waitingTime(0), m_state(state)
 {
 	if (state == arriving)
 		this->m_timeRemaining = rand() % MAX_TIME + 1;
 	else
 		this->m_timeRemaining = -1;
+}
+
+void Plane::refresh()
+{
+	if (this->m_state == arriving)
+		this->m_timeRemaining--;
+	this->m_waitingTime++;
 }
--- Data_Structure_FinalProject_1/Plane.h
+++ Data_Structure_FinalProject_1/Plane.h
@@ -6,28 +6,28 @@ using std::ostream;
 using std::endl;
 using std::pair;
 
-enum Plane_states//飞机状态(可能用不到)
+enum Plane_states	//飞机状态
 {
-	null,		//无状态
-	arriving,	//准备降落
-	departing	//准备起飞
+	arriving,		//准备降落
+	departing		//准备起飞
 };
 
 class Plane
 {
 	friend ostream& operator<< (ostream& out, const Plane& p);				//打印飞机信息
 public:
-	Plane(int id, int t)													//传入飞机编号与剩余时间构造
-		:m_id(id), m_timeRemaining(t), m_waitingTime(0) {}
+	Plane(int id, int t);													//传入飞机编号与剩余时间构造
 	Plane(int id, Plane_states state);
 
-	void refresh() { this->m_timeRemaining--; }								//刷新飞机剩余时间
+	void refresh();															//刷新飞机时间
 
 	bool check() const { return this->m_timeRemaining == EMERGENCY_TIME; }	//飞机警报
 
 
 private:
 	int m_id;																//飞机编号
+	Plane_states m_state;													//飞机状态
+
 	int m_timeRemaining;													//剩余时间(-1表示飞机处于准备起飞状态)
 	int m_waitingTime;														//等待时间
 };
