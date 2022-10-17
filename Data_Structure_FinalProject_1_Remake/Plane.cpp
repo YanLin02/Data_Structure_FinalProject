4f1b5b2b61c18f9d9919e45edf5d78eead9f12aa
Sun Yanlin <1310400098@qq.com>
2022-10-17 16:28:42 +0800

    添加原有飞机类

---
+++ Data_Structure_FinalProject_1_Remake/Plane.cpp
@@ -0,0 +1,55 @@
+#include "Plane.h"
+
+ostream& operator<<(ostream& out, const Plane& p)
+{
+	out << " + 编号为：" << p.m_id << " 的飞机";
+	if (p.m_state == landing)
+		out << "尚未降落，已经等待：" << p.m_waitingTime << " 单位时间，剩余油量为：" << p.m_timeRemaining << endl;
+	else
+		out << "等待起飞，已经等待：" << p.m_waitingTime << " 单位时间" << endl;
+	return out;
+}
+
+Plane::Plane()
+	:m_id(-1), m_timeRemaining(-1), m_waitingTime(-1), m_state(null) {}
+
+Plane::Plane(int id, int t)
+	:m_id(id), m_timeRemaining(t), m_waitingTime(0)
+{
+	if (t < 0)
+		this->m_state = departing;
+	else
+		this->m_state = landing;
+}
+
+
+Plane::Plane(int id, Plane_states state)
+	:m_id(id), m_waitingTime(0), m_state(state)
+{
+	if (state == landing)
+		this->m_timeRemaining = rand() % MAX_TIME + 1;
+	else
+		this->m_timeRemaining = -1;
+}
+
+void Plane::refresh()
+{
+	if (this->m_state == landing)
+		this->m_timeRemaining--;
+	this->m_waitingTime++;
+}
+
+void Plane::emergencyLanding(ostream& out)const
+{
+	out << this->m_id << "号飞机紧急降落！\n";
+}
+
+void Plane::land(ostream& out)const
+{
+	out << this->m_id << "号飞机降落，等待时间为：" << this->m_waitingTime << " 剩余油量为：" << this->m_timeRemaining << endl;
+}
+
+void Plane::depart(ostream& out)const
+{
+	out << this->m_id << "号飞机起飞，等待时间为：" << this->m_waitingTime << endl;
+}
---
+++ Data_Structure_FinalProject_1_Remake/Plane.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include "Setting.h"
+//using std::ostream;
+//using std::endl;
+//using std::pair;
+
+enum Plane_states	//飞机状态
+{
+	null,			//无状态
+	landing,		//准备降落
+	departing		//准备起飞
+};
+
+class Plane
+{
+	friend ostream& operator<< (ostream& out, const Plane& p);				//打印飞机信息
+public:
+	Plane();																//默认构造（队列用）
+	Plane(int id, int t);													//传入飞机编号与剩余时间构造
+	Plane(int id, Plane_states state);
+
+	void refresh();															//刷新飞机时间
+
+	bool check() const { return this->m_timeRemaining <= EMERGENCY_TIME; }	//飞机警报
+
+	void emergencyLanding(ostream& out)const;								//紧急降落
+	void land(ostream& out)const;											//普通降落
+	void depart(ostream& out)const;											//起飞
+
+	int getTimeRemaining()const { return this->m_timeRemaining; }
+
+private:
+	int m_id;																//飞机编号
+	Plane_states m_state;													//飞机状态
+
+	int m_timeRemaining;													//剩余时间(-1表示飞机处于准备起飞状态)
+	int m_waitingTime;														//等待时间
+};
+
